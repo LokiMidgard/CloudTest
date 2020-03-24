6cdde76a9eec952c4a5975fba7e5882d8569c09f
Patrick Kranz <p.kranz@seeburger.de>
2020-03-24 15:17:58 +0100

    removed unnesesarry code.

--- CloudTest/Utilities.h
+++ CloudTest/Utilities.h
@@ -11,9 +11,6 @@ class Utilities
 {
 public:
     static HRESULT AddFolderToSearchIndexer(_In_ PCWSTR folder);
-    static void ApplyTransferStateToFile(_In_ PCWSTR fullPath, _In_ CF_CONNECTION_KEY& connectionKey, _In_ CF_TRANSFER_KEY& transferKey, UINT64 total, UINT64 completed);
-	static void ApplyTransferStateToFile(PCWSTR fullPath, SYNC_TRANSFER_STATUS& status);
-	static void ApplyCustomStateToPlaceholderFile(_In_ PCWSTR path, _In_ PCWSTR filename, _In_ winrt::StorageProviderItemProperty& prop);
     
     static winrt::com_array<wchar_t> ConvertSidToStringSid(_In_ PSID sid)
     {
@@ -27,35 +24,4 @@ public:
             throw std::bad_alloc();
         }
     };
-
-    inline static LARGE_INTEGER FileTimeToLargeInteger(_In_ const FILETIME fileTime)
-    {
-        LARGE_INTEGER largeInteger;
-
-        largeInteger.LowPart = fileTime.dwLowDateTime;
-        largeInteger.HighPart = fileTime.dwHighDateTime;
-
-        return largeInteger;
-    }
-
-    inline static LARGE_INTEGER LongLongToLargeInteger(_In_ const LONGLONG longlong)
-    {
-        LARGE_INTEGER largeInteger;
-        largeInteger.QuadPart = longlong;
-        return largeInteger;
-    }
-
-    inline static CF_OPERATION_INFO ToOperationInfo(
-        _In_ CF_CALLBACK_INFO const* info,
-        _In_ CF_OPERATION_TYPE operationType)
-    {
-        return
-            CF_OPERATION_INFO
-        {
-            sizeof(CF_OPERATION_INFO),
-            operationType,
-            info->ConnectionKey,
-            info->TransferKey
-        };
-    }
 };
